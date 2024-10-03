97a19492479ce5e684c4dbb766a6a40ebfb92ae7
Masaki <pataporasu@gmail.com>
2024-10-03 03:19:50 +0100

    add picamera2 as a submodule

---
+++  Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi/Code/Server-pi5/Motor.py
@@ -0,0 +1,87 @@
+import time
+import math
+from PCA9685 import PCA9685
+from ADC import *
+
+class Motor:
+    def __init__(self):
+        self.pwm = PCA9685(0x40, debug=True)
+        self.pwm.setPWMFreq(50)
+        self.time_proportion = 3  # ????????
+        self.adc = Adc()
+
+    def duty_range(self, duty1, duty2, duty3, duty4):
+        duty1 = max(min(duty1, 4095), -4095)
+        duty2 = max(min(duty2, 4095), -4095)
+        duty3 = max(min(duty3, 4095), -4095)
+        duty4 = max(min(duty4, 4095), -4095)
+        return duty1, duty2, duty3, duty4
+
+    def left_Upper_Wheel(self, duty):
+        if duty > 0:
+            self.pwm.setMotorPwm(0, 0)
+            self.pwm.setMotorPwm(1, duty)
+        elif duty < 0:
+            self.pwm.setMotorPwm(1, 0)
+            self.pwm.setMotorPwm(0, abs(duty))
+        else:
+            self.pwm.setMotorPwm(0, 0)
+            self.pwm.setMotorPwm(1, 0)
+
+    def left_Lower_Wheel(self, duty):
+        if duty > 0:
+            self.pwm.setMotorPwm(3, 0)
+            self.pwm.setMotorPwm(2, duty)
+        elif duty < 0:
+            self.pwm.setMotorPwm(2, 0)
+            self.pwm.setMotorPwm(3, abs(duty))
+        else:
+            self.pwm.setMotorPwm(2, 0)
+            self.pwm.setMotorPwm(3, 0)
+
+    def right_Upper_Wheel(self, duty):
+        if duty > 0:
+            self.pwm.setMotorPwm(6, 0)
+            self.pwm.setMotorPwm(7, duty)
+        elif duty < 0:
+            self.pwm.setMotorPwm(7, 0)
+            self.pwm.setMotorPwm(6, abs(duty))
+        else:
+            self.pwm.setMotorPwm(6, 0)
+            self.pwm.setMotorPwm(7, 0)
+
+    def right_Lower_Wheel(self, duty):
+        if duty > 0:
+            self.pwm.setMotorPwm(4, 0)
+            self.pwm.setMotorPwm(5, duty)
+        elif duty < 0:
+            self.pwm.setMotorPwm(5, 0)
+            self.pwm.setMotorPwm(4, abs(duty))
+        else:
+            self.pwm.setMotorPwm(4, 0)
+            self.pwm.setMotorPwm(5, 0)
+
+    def setMotorModel(self, duty1, duty2, duty3, duty4):
+        duty1, duty2, duty3, duty4 = self.duty_range(duty1, duty2, duty3, duty4)
+        self.left_Upper_Wheel(duty1)
+        self.left_Lower_Wheel(duty2)
+        self.right_Upper_Wheel(duty3)
+        self.right_Lower_Wheel(duty4)
+
+    def Rotate(self, direction, speed=2000):
+        """
+        direction: 'left' ??? 'right'
+        speed: ????????0?4095?
+        """
+        if direction == 'right':
+            # ???????????????
+            self.setMotorModel(speed, speed, -speed, -speed)
+        elif direction == 'left':
+            # ???????????????
+            self.setMotorModel(-speed, -speed, speed, speed)
+        else:
+            # ??
+            self.setMotorModel(0, 0, 0, 0)
+
+    def stop(self):
+        self.setMotorModel(0, 0, 0, 0)
