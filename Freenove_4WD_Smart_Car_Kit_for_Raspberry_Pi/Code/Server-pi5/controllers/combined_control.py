38a726d01afe5344ac3b7f75aae22613944293a5
Masaki Ogawa <pataporasu@gmail.com>
2024-10-04 12:44:18 +0900

    update

--- Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi/Code/Server-pi5/controllers/combined_control.py
+++ Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi/Code/Server-pi5/controllers/combined_control.py
@@ -13,7 +13,7 @@ import pygame
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
 class Buzzer:
-    def __init__(self, sound_file='/正しい/パス/maou_se_system49.wav', volume=0.7):
+    def __init__(self, sound_file='/home/ogawamasaki/School-Internship-3th-Car/ Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi/Code/Server-pi5/data/maou_se_system49.wav', volume=0.7):
         # sudoでの実行を防止
         if os.geteuid() == 0:
             logging.error("Running as sudo is not allowed. Please run without sudo.")
@@ -73,7 +73,7 @@ def combined_control():
         parameters.cornerRefinementMethod = aruco.CORNER_REFINE_SUBPIX
 
         # ブザーの初期化
-        buzzer = Buzzer(sound_file='/正しい/パス/maou_se_system49.wav')
+        buzzer = Buzzer(sound_file='/home/ogawamasaki/School-Internship-3th-Car/Freenove_4WD_Smart_Car_Kit_for_Raspberry_Pi/Code/Server-pi5/data/maou_se_system49.wav')
 
         # FPS計算の初期化
         frame_count = 0
